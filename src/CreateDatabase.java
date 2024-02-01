49bc749ee3732c31c1cfa9015b7dc58def07eaba
Dhruv <chodvadiyadhruv06@gmail.com>
2024-02-01 21:00:30 +0530

    JDBC

--- src/CreateDatabase.java
+++ src/CreateDatabase.java
@@ -1,18 +1,22 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.Statement;
 
 public class CreateDatabase {
     public static void main(String[] args) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection con= DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/newdata","root","root");
+                    "jdbc:mysql://localhost:3306/","root","root");
+            Statement stmt = con.createStatement();
+            String sql = "CREATE DATABASE STUDENTS";
+            stmt.executeUpdate(sql);
+
             if(con.isClosed()){
                 System.out.println("closed");
             }else {
-                System.out.println("opan");
+                System.out.println("Create Students Database");
             }
-
         } catch(Exception e){
             System.out.println(e);
         }
