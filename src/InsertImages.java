1a17f60c93c580d64645cc55cb3c4dce163846d1
Dhruv <chodvadiyadhruv06@gmail.com>
2024-02-02 21:27:36 +0530

    JDBC

---
+++ src/InsertImages.java
@@ -0,0 +1,31 @@
+import java.io.FileInputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class InsertImages {
+    public static void main(String[] args) {
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection con= DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/newdata","root","root");
+
+            String q = "insert into Images(pic) values (?)";
+
+            java.sql.PreparedStatement pstmt = con.prepareStatement(q);
+
+            FileInputStream fis = new FileInputStream("C:\\Users\\macChoice\\Downloads\\wallpaper.jpg");
+
+            pstmt.setBinaryStream(1,fis,fis.available());
+
+            pstmt.executeUpdate();
+
+            System.out.println("Done...");
+
+            con.close();
+
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+}
