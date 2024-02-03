aff7f3ba384ed0bf269c4bb6be79a4182df62cef
Dhruv <chodvadiyadhruv06@gmail.com>
2024-02-03 21:06:50 +0530

    JDBC

---
+++ src/ConnectionProvider.java
@@ -0,0 +1,22 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConnectionProvider {
+    public static Connection con;
+
+    public static Connection getConnection(){
+        try {
+            if(con==null){
+                Class.forName("com.mysql.cj.jdbc.Driver");
+                con= DriverManager.getConnection(
+                        "jdbc:mysql://localhost:3306/newdata","root","root");
+            }
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+
+        return con;
+    }
+}
+
